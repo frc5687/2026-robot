e7bced556ea97cc9845b008d763fd15dbbaeb08f
b1llb33 <adamddustin@gmail.com>
2026-03-25 11:15:53 -0400

    header file wasn't included

---
+++ src/main/include/commands/drive/SlowModeCommand.h
@@ -0,0 +1,24 @@
+// Team 5687 2026
+
+#pragma once
+
+#include <frc2/command/Command.h>
+#include <frc2/command/CommandHelper.h>
+#include <units/voltage.h>
+
+#include "subsystem/drive/DriveSubsystem.h"
+
+class SlowModeCommand : public frc2::CommandHelper<frc2::Command, SlowModeCommand> {
+public:
+  SlowModeCommand(
+    DriveSubsystem *drive);
+
+  void Initialize() override;
+  void Execute() override;
+  void End(bool interrupted) override;
+  bool IsFinished() override;
+
+private:
+  DriveSubsystem *m_drive;
+
+};
